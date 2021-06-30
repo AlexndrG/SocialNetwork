d98227a8aefc77654a5db307b19a4a24de40016d
AlexndrG <alexndr1@mail.ru>
2021-06-30 13:24:33 +0300

    new logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,8 @@ import s from './Header.module.css';
 export const Header = () => {
     return (
         <header className={s.header}>
-            <img src="http://www.logobook.com/wp-content/uploads/2016/10/birds.svg"/>
+            {/*<img src="http://www.logobook.com/wp-content/uploads/2016/10/birds.svg"/>*/}
+            <img src="http://vectorlogofree.com/wp-content/uploads/2014/09/53354-bird-handmade-logo-icon-vector-icon-vector-eps.png" alt={'logo'}/>
         </header>
     )
 }
