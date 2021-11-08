a16aae7b8a3f69f0fb2ab7b618dcf3ae54a9fdf7
AlexndrG <alexndr1@mail.ru>
2021-11-08 19:19:22 +0300

    90 - redux-ducks рефакторинг - React JS (Paginator corrections)

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {RefObject, useEffect} from 'react';
 import s from './Paginator.module.css';
 
 
@@ -10,19 +10,35 @@ type PaginatorPropsType = {
 }
 
 export const Paginator = (props: PaginatorPropsType) => {
+    useEffect(() => {
+        if (refs[props.currentPage]) {
+            const index = props.currentPage > 5
+                ? refs.length >= props.currentPage + 5 ? props.currentPage + 5 : refs.length-1
+                : props.currentPage-1
+            alert(index)
+            const ref = refs[index].current
+            if (ref) {
+                ref.scrollIntoView()
+            }
+        }
+    }, [])
+
     const pagesCount = Math.ceil(props.totalUserCount / props.pageSize)
     const pages: Array<number> = []
+    const refs: Array<RefObject<HTMLDivElement>> = []
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
+        refs.push(React.createRef<HTMLDivElement>())
     }
 
     return (
         <div className={s.paginator}>
             <div className={s.main}>
                 {
-                    pages.map(p =>
+                    pages.map((p,i) =>
                         <div
                             key={p}
+                            ref={refs[i]}
                             className={s.page + ' ' + (props.currentPage === p ? s.selectedPage : '')}
                             onClick={(event) => props.onPageChanged(p)}>
                             {` ${p}`}
